ecb7d2e1fd53035b2552815f6f83146a4e4a6df9
Matheus <tambosi.matheus@gmail.com>
2020-08-05 00:44:21 -0300

    Animations and Rigidbody Integration
    Finished polishing main animations and started integrating the code and the animations with Rigidbody for implementation of the gameplay

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public Animator anim;
+
+    public float jumpPower = 5f;
+    public Transform startPos;
+    private void Start()
+    {        
+        rb = GetComponent<Rigidbody>();
+        rb.detectCollisions = true;
+        transform.position = startPos.position;
+        transform.rotation = startPos.rotation;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    public void OnAir()
+    {
+        anim.enabled = false;
+        //rb.detectCollisions = true;
+        //rb.useGravity = true;
+        rb.AddForce((Vector3.up * jumpPower), ForceMode.Impulse);        
+    }
+}
